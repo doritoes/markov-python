f91deeef5010a150aa6e14f0a50a741748ce1518
doritoes <39832079+doritoes@users.noreply.github.com>
2018-06-01 14:52:30 -0400

    Update markov.py

--- markov.py
+++ markov.py
@@ -81,7 +81,7 @@ class Markov:
 
     def ask(self, seed):
         """Queries the Markov chain with the seed of your choice (make sure it is an n-element list/tuple where n is the order of the chain)."""
-        tmp = seed
+        tmp = tuple(seed)
         if not tmp in self.states:
             tmp = self.init_chain()
         t = self.states[tmp][random.randint(0, len(self.states[tmp])-1)]
